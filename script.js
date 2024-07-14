753ebacf41ab7b26229e9dea8a0f025bcf68ed25
Invincible057 <118792929+Invincible057@users.noreply.github.com>
2024-07-14 23:27:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let display = document.getElementById('display');
+let currentNumber = '';
+let previousNumber = '';
+let operation = undefined;
+
+function appendNumber(number) {
+    currentNumber += number;
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentNumber === '') return;
+    if (previousNumber !== '') {
+        calculate();
+    }
+    operation = op;
+    previousNumber = currentNumber;
+    currentNumber = '';
+}
+
+function calculate() {
+    let result;
+    const prev = parseFloat(previousNumber);
+    const current = parseFloat(currentNumber);
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (operation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentNumber = result;
+    operation = undefined;
+    previousNumber = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentNumber = '';
+    previousNumber = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.innerText = currentNumber;
+}
